7ffba856369712d70aa42dd53750dc5096c10123
Klaus Vancamelbeke <klaus.vancamelbek@gmail.com>
2024-12-25 17:24:26 +0100

    DnsServer project

---
+++ devices/Dns/DnsServer/DnsServer.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.MulticastDns
+{
+    /// <summary>
+    /// DnsServer monitors for incoming DNS questions and answers accordingly.
+    /// </summary>
+    public sealed class DnsServer
+    {
+    }
+}
